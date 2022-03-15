00171e786a50adfe20d2750ce6508c890cc42774
Megha Dogra <76041569+Megha-prog@users.noreply.github.com>
2022-03-15 05:44:00 +0530

    Add files via upload

--- models/index.js
+++ models/index.js
@@ -9,36 +9,73 @@
  * For example : require(./models); // No need to specify the file name index.js
  */
 
-const config = require("../configs/db.config");
-const Sequelize = require("sequelize");
-
-/**
- * Creating the DB connection
- */
-const sequelize = new Sequelize(
-    config.DB,
-    config.USER,
-    config.PASSWORD,
-
-
-    {
-        // change this to zero
-        host: config.HOST,
-        dialect: config.dialect,
-        operatorsAliases: 0,
-        pool: {
-            max: config.pool.max,
-            min: config.pool.min,
-            acquire: config.pool.acquire,
-            idle: config.pool.idle
-        }
-    }
-);
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-db.category = require('./category.model.js')(sequelize, Sequelize);
-db.product = require('./product.model.js')(sequelize, Sequelize);
-
-
-module.exports = db;
+ const config = require("../configs/db.config");
+ const Sequelize = require("sequelize");
+ 
+ /**
+  * Creating the DB connection
+  */
+ const sequelize = new Sequelize(
+     config.DB,
+     config.USER,
+     config.PASSWORD,
+     {
+         host: config.HOST,
+         dialect: config.dialect,
+         operatorsAliases: false,
+         pool: {
+             max: config.pool.max,
+             min: config.pool.min,
+             acquire: config.pool.acquire,
+             idle: config.pool.idle
+         }
+     }
+ );
+ 
+ const db = {};
+ db.Sequelize = Sequelize;
+ db.sequelize = sequelize;
+ db.category = require('./category.model.js')(sequelize, Sequelize);
+ db.product = require('./product.model.js')(sequelize, Sequelize);
+ db.user = require('./user.model.js')(sequelize, Sequelize);
+ db.role = require('./role.model.js')(sequelize, Sequelize);
+ db.cart = require('./cart.model.js')(sequelize, Sequelize);
+ 
+ 
+ /**
+    * Establishing the relationship between Role and User
+    */
+ db.role.belongsToMany(db.user, {
+     through: "user_roles",
+     foreignKey: "roleId",
+     otherKey: "userId"
+ });
+ db.user.belongsToMany(db.role, {
+     through: "user_roles",
+     foreignKey: "userId",
+     otherKey: "roleId"
+ });
+ 
+ /**
+  * Establishing the relationship between Cart and User
+  */
+  db.user.hasMany(db.cart);
+ 
+  /**
+   * Establishing the relationship between Cart and Items : Many to Many
+   */
+   db.product.belongsToMany(db.cart, {
+     through: "cart_products",
+     foreignKey: "productId",
+     otherKey: "cartId"
+ });
+ db.cart.belongsToMany(db.product, {
+     through: "cart_products",
+     foreignKey: "cartId",
+     otherKey: "productId"
+ });
+ 
+ db.ROLES = ["user", "admin"];
+ 
+ 
+ module.exports = db;
